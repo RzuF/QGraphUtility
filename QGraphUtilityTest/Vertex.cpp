dbb89baf4c3098446df428e0a7238aa437373805
RzuF <rzuf22@gmail.com>
2017-03-01 23:18:40 +0100

    Fixed unnecessary definitions

--- QGraphUtilityTest/Vertex.cpp
+++ QGraphUtilityTest/Vertex.cpp
@@ -2,8 +2,6 @@
 
 Vertex::Vertex(int id, QString name) : _id(id), _edges(QList<Edge*>()), _name(name)
 {
-	_id = id;	
-	_name = name;
 }
 
 Vertex::Vertex(int id, QList<Edge*> edges, QString name) : _id(id), _edges(edges), _name(name)
